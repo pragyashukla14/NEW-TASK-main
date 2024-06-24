6ba165c73a87c8102a74f7b870577a8beddec8fd
pragyashukla14 <pragya.shukla@zeuslearning.com>
2024-06-24 12:22:59 +0530

    new

--- NEW-TASK-main/script.ts
+++ NEW-TASK-main/script.ts
@@ -1,17 +1,17 @@
-var alertbutton: HTMLElement | null = document.getElementById('alertButton');
-if (alertbutton) {
-    alertbutton.addEventListener('mouseover', function(this: HTMLElement) 
-    {
-        this.style.backgroundColor = 'black';
-    });
-    alertbutton.addEventListener('mouseout', function(this: HTMLElement) 
-    {
-        this.style.backgroundColor = '';
-    });
-}
-else {
-    console.log("Button not found");
-}
+// var alertbutton: HTMLElement | null = document.getElementById('alertButton');
+// if (alertbutton) {
+//     alertbutton.addEventListener('mouseover', function(this: HTMLElement) 
+//     {
+//         this.style.backgroundColor = 'black';
+//     });
+//     alertbutton.addEventListener('mouseout', function(this: HTMLElement) 
+//     {
+//         this.style.backgroundColor = '';
+//     });
+// }
+// else {
+//     console.log("Button not found");
+// }
 
 
 
